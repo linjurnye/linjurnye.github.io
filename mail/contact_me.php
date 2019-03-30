6d88181fe5537a235754072604f98b47c1285965
linjurnye <purple1520@vip.qq.com>
2019-03-31 02:02:10 +0800

    个人主页

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,10 +14,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // 创建电子邮件并发送邮件
-$to = 'linjurnye96@163.com'; // 将您的电子邮件地址添加到“”之间，替换为yourname@yourdomain.com-这是表单将向其发送邮件的位置。
+$to = 'linjurnye96@163.com'; // 将您的电子邮件地址添加到“”之间，替换为自己的邮箱-这是表单将向其发送邮件的位置。
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: linjurnye96@163.com\n"; // 这是生成的邮件将来自的电子邮件地址。我们建议使用noreply@yourdomain.com。
+$headers = "From: linjurnye96@163.com\n"; // 这是生成的邮件将来自的电子邮件地址。我们建议使用自己的邮箱。
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
